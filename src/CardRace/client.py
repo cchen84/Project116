6265fbd2681595adc2829ec8fd374bae0f11db88
christine <cchen84@buffalo.edu>
2019-03-06 20:18:04 -0500

    New unit Test, new boundaries function,

--- src/CardRace/client.py
+++ src/CardRace/client.py
@@ -1,4 +1,6 @@
 import pygame
+from tkinter import *
+from tkinter import messagebox
 
 width = 750
 height = 750
@@ -20,25 +22,49 @@ class Player():
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
 
-# Function to move character left, right, up, down
+# Function to move character left, right, up, down (boundaries included)
     def move(self):
         keys = pygame.key.get_pressed()
+        left = keys[pygame.K_LEFT]
 
-        if keys[pygame.K_LEFT] and self.x > 0:
+        if left:
             self.x -= self.val
 
-        if keys[pygame.K_RIGHT] and self.x < width - self.width:
+        if keys[pygame.K_RIGHT]:
             self.x += self.val
 
-        if keys[pygame.K_UP] and self.y < 0:
+        if keys[pygame.K_UP]:
             self.y -= self.val
 
-        if keys[pygame.K_DOWN] and self.y > height - self.height:
+        if keys[pygame.K_DOWN]:
             self.y += self.val
 
+#Function to set boundaries
+    def boundaries(self):
+        if self.x < 0:
+            self.x = 0
+
+        if self.x > width - self.width:
+            self.x = width - self.width
+
+        if self.y < 0:
+            self.y = 0
+
+        if self.y > height - self.height:
+            self.y = height - self.height
+
+# Draws Winner message to screen
+        def drawWinner():
+            pygame.font.init()
+            font = pygame.font.SysFont("arial", 30, True)
+            textSurface = font.render("Winner", 1, (0, 0, 0))
+            win.blit(textSurface, (375, 375))
+            pygame.display.update()
 
 
-
+#Checks for Winner
+        if self.x == 0:
+            drawWinner()
 
 
 
