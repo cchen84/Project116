c8539614a847c8a1b524f8b41d5b98f7184b62e6
christine <cchen84@buffalo.edu>
2019-03-07 13:02:19 -0500

    new file

--- src/CardRace/client.py
+++ src/CardRace/client.py
@@ -1,6 +1,5 @@
 import pygame
-from tkinter import *
-from tkinter import messagebox
+
 
 width = 750
 height = 750
@@ -8,6 +7,9 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Card Race")
 
 clientNumber = 0
+win_condition = False
+
+
 
 class Player():
     def __init__(self, x, y, width, height, color):
@@ -22,12 +24,14 @@ class Player():
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
 
-# Function to move character left, right, up, down (boundaries included)
+        self.rect = (self.x, self.y, self.width, self.height)
+        pygame.display.update()
+
+# Function to move character left, right, up, down
     def move(self):
         keys = pygame.key.get_pressed()
-        left = keys[pygame.K_LEFT]
 
-        if left:
+        if keys[pygame.K_LEFT]:
             self.x -= self.val
 
         if keys[pygame.K_RIGHT]:
@@ -39,8 +43,8 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.val
 
-#Function to set boundaries
-    def boundaries(self):
+#Setting game boundaries
+
         if self.x < 0:
             self.x = 0
 
@@ -53,25 +57,21 @@ class Player():
         if self.y > height - self.height:
             self.y = height - self.height
 
-# Draws Winner message to screen
-        def drawWinner():
-            pygame.font.init()
-            font = pygame.font.SysFont("arial", 30, True)
-            textSurface = font.render("Winner", 1, (0, 0, 0))
-            win.blit(textSurface, (375, 375))
-            pygame.display.update()
-
-
-#Checks for Winner
-        if self.x == 0:
-            drawWinner()
 
 
 
 
+    def winner(self):
+        run = True
+        while run:
+            pygame.init()
+            win = pygame.display.set_mode((750, 750))
+            winner_font = pygame.font.Font(None, 70)
+            winner_text = winner_font.render("WINNER", True, (0, 0, 0))
+            win.blit(winner_text, (60, 384))
+            pygame.display.flip()
 
-        self.rect = (self.x, self.y, self.width, self.height)
-        pygame.display.update()
+
 
 
 def redrawWindow(win,player):
@@ -79,17 +79,19 @@ def redrawWindow(win,player):
     player.draw(win)
     pygame.display.update()
  
-
+#GAME LOOP
 def main():
     run = True
     p = Player(50,50,100,100,(0,255,0))
     clock = pygame.time.Clock()
     while run:
         clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+
         p.move()
         redrawWindow(win, p)
 
---
+++ src/CardRace/game.py
@@ -0,0 +1,161 @@
+import pygame
+
+pygame.init()
+
+# Window dimension
+display_width = 600
+display_height = 700
+
+# Colors
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+COBALT = (61, 89, 171)
+
+
+
+screen = pygame.display.set_mode((display_width, display_height))
+clock = pygame.time.Clock()
+
+
+
+# Game set up
+def game_init():
+
+
+    pygame.display.set_caption("Card Race")
+    screen.fill(WHITE)
+
+
+
+# PLAYER CLASS
+class Player():
+
+
+    # INITIALIZE PLAYER'S COORDINATES, SIZE, COLOR
+    def __init__(self, color):
+        self.color = color
+        self.velocity = 3
+        self.x = 10
+        self.y = 10
+        self.left = False
+        self.right = False
+        self.width = 50
+        self.height = 50
+
+    # DRAW THE CHARACTER
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        pygame.display.update()
+
+
+class attack():
+
+
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.velocity = 10
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+
+
+
+#Drawing on screen
+def drawWindow(screen, player):
+    screen.fill(BLACK)
+    player.draw(screen)
+
+    for  in arrows:
+        bullet.draw(screen)
+
+    pygame.display.update()
+
+
+
+
+# GAME LOOP
+player1 = Player(COBALT)
+bullets = []
+def game_loop():
+
+    run_game = True
+
+    while run_game:
+
+        clock.tick(100)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+
+        for bullet in bullets:
+            if bullet.x < display_width and bullet.x > 0:
+                bullet.x += bullet.velocity
+            else:
+                bullets.pop(bullets.index(bullet)) #bullet gone from screen when hits boundary
+
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            player1.x -= player1.velocity
+
+        elif keys[pygame.K_RIGHT]:
+            player1.x += player1.velocity
+
+        elif keys[pygame.K_UP]:
+            player1.y -= player1.velocity
+
+        elif keys[pygame.K_DOWN]:
+            player1.y += player1.velocity
+
+        if keys[pygame.K_SPACE]:
+
+            if len(bullets) < 3:
+                bullets.append(attack(round(player1.x + player1.width//2), round(player1.y + player1.height//2), 6 , RED))
+
+
+        # SETTING BOUNDARIES
+        if player1.x < 0:
+            player1.x = 0
+
+        if player1.x > display_width - player1.width:
+            player1.x = display_width - player1.width
+
+        if player1.y < 0:
+            player1.y = 0
+
+        if player1.y > display_height - player1.height:
+            player1.y = display_height - player1.height
+
+
+
+        drawWindow(screen, player1)
+
+
+
+    pygame.display.quit()
+    pygame.quit()
+
+
+def main():
+    game_init()
+    game_loop()
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
