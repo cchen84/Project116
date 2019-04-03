3fd7554fcf4132a888ed84d550b04cfbcd7c229b
meyou <sifulsid@buffalo.edu>
2019-04-03 19:22:30 -0400

    create testing

--- src/CardRace/Database.py
+++ src/CardRace/Database.py
@@ -1,10 +1,9 @@
 import csv
-import json
 
 
 def allUsers(filename):
     users = []
-    with open( filename, newline="") as f:
+    with open(filename, newline="") as f:
         for row in csv.reader(f):
             users.append(row[0])
     return users
@@ -13,13 +12,13 @@ def allUsers(filename):
 def add_Users(filename, name):
     x = allUsers(filename)
     new_user = name
-    if (new_user not in x):
+    if name is "":
+        return x
+    elif new_user not in x:
         with open(filename, "a") as f:
             csv.writer(f).writerow([new_user])
             x.append(new_user)
         return x
     else:
         return x
-
-
 
